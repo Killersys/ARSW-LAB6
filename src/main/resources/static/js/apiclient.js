56a78bcc55239bc67e8810bf848b790c7bbdb087
lale1507 <laurisaleja15.15@gmail.com>
2021-03-10 22:33:47 -0500

    Modifcaciones front

---
+++ src/main/resources/static/js/apiclient.js
@@ -0,0 +1,41 @@
+var apiUrl = "http://localhost:8080/blueprints/"
+apiclient = (function() {
+    return {
+        getBlueprintsByAuthor: function(name, callback) {
+            jQuery.ajax({
+                url: apiUrl + name,
+                success: function (result) {
+                    callback(result);
+                },
+                async: true
+            });
+        },
+        getBlueprintsByNameAndAuthor: function(author, name, callback) {
+            jQuery.ajax({
+                url: apiUrl+name+"/"+author,
+                success: function (result) {
+                    callback(result);
+                },
+                async: true
+            });
+        },
+        setBlueprint: function(author, name, newPlan) {
+
+            var putPromise = $.ajax({
+              url: "/blueprints/" + author + "/" + name + "/",
+              type: "PUT",
+              data: newPlan,
+              contentType: "application/json"
+            });
+            putPromise.then(
+              function() {
+                console.log(putPromise);
+                app.getBlueprintsByAuthor(author);
+              },
+              function() {
+                console.info("Error");
+              }
+            );
+          }
+    };
+})();
